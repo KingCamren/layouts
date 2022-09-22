b6a232a524fcc9cfb42274f63517b45fcbc05a91
1713564 <king.carrot.171@gmail.com>
2022-09-22 11:51:48 -0400

    Initial commit

---
+++ app/src/main/java/com/zmatthews/layouts/MainActivity.java
@@ -0,0 +1,36 @@
+package com.zmatthews.layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void navigateLayoutCorners(View view) {
+    }
+
+    public void navigateLayoutChecker(View view) {
+    }
+
+    public void navigateLayoutGuideline(View view) {
+    }
+
+    public void navigateLayoutOverlap(View view) {
+    }
+
+    public void navigateLayoutScrollText(View view) {
+    }
+
+    public void navigateLayoutScrollButtons(View view) {
+    }
+
+    public void navigateLayoutScrollCheckboxes(View view) {
+    }
+}
